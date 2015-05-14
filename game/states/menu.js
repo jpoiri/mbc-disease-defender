992cacd786de9b27cd3e06764e9e986b538d3f03
jpoiri <justin.poirier@gmail.com>
2015-05-14 10:23:49 -0300

    Updated menu

--- game/states/menu.js
+++ game/states/menu.js
@@ -4,37 +4,48 @@ function Menu() {}
 
 Menu.prototype = {
   preload: function() {
-    this.game.load.bitmapFont('retro', 'assets/font.png', 'assets/font.fnt');
+
+    // Load bitmap fonts.
+    this.game.load.bitmapFont('new-york-escape-3d',
+      'assets/fonts/new-york-escape-3d.png', 'assets/fonts/new-york-escape-3d.fnt');
+    this.game.load.bitmapFont('new-york-escape-cond',
+      'assets/fonts/new-york-escape-cond.png', 'assets/fonts/new-york-escape-cond.fnt');
+    this.game.load.bitmapFont('new-york-escape-grad',
+      'assets/fonts/new-york-escape-grad.png', 'assets/fonts/new-york-escape-grad.fnt');
   },
   create: function() {
 
-
+    // Set stage color.
     this.game.stage.backgroundColor = '#FFFFFF';
 
-    this.name = this.game.add.bitmapText(this.game.world.centerX,
-      this.game.world.centerY, 'retro', 'Disease Defender +', 64);
+    this.diseaseTxt = this.game.add.bitmapText(this.game.world.centerX,
+      (this.game.world.centerY - 100), 'new-york-escape-cond', 'Disease', 110);
 
-    this.name.anchor.set(0.5);
+    this.diseaseTxt.anchor.set(0.5);
 
-    this.start = this.name = this.game.add.bitmapText(this.game.world.centerX,
-      (this.game.world.centerY + 100), 'retro', 'Click to start to begin', 30);
+    this.defenderTxt = this.game.add.bitmapText(this.game.world.centerX,
+      this.game.world.centerY, 'new-york-escape-cond', 'Defender+', 110);
+    this.defenderTxt.anchor.set(0.5);
 
-    this.start.anchor.set(0.5);
 
-    /*
-    this.name = this.game.add.text(this.game.world.centerX,
-      this.game.world.centerY,
-      'Medavie Disease Defender', {
-      font: '65px arial',
-      fill: '#0078A7',
-      align: 'center'
-    });
+    this.startTxt =  this.game.add.text(this.game.world.centerX, (this.game.world.centerX + 100),
+      'Click to start defending', {
+        font: '30px arial', fill: '#0078A7'})
 
-    this.name.anchor.set(0.5);
-    this.name.setShadow(0, 0, 'rgba(0, 0, 0, 0.5)', 0);
-    */
+    //this.startTxt = this.game.add.bitmapText(this.game.world.centerX,
+      //(this.game.world.centerY + 100), 'new-york-escape-cond', 'Click to start defending', 25);
+
+    this.startTxt.anchor.set(0.5);
 
   },
+
+  createLato: function() {
+
+    this.startTxt =  this.game.add.text(this.game.world.centerX, (this.game.world.centerX + 100),
+      'Test', {
+        font: '30px Lato', fill: '#0078A7'})
+  },
+
   update: function() {
     if(this.game.input.activePointer.justPressed()) {
       this.game.state.start('play');
