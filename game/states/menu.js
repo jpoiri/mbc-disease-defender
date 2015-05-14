92987154557240dd4bcde33fd8d0ecd61f0bfbc0
jpoiri <justin.poirier@gmail.com>
2015-05-14 16:30:55 -0300

    added alpha transition

--- game/states/menu.js
+++ game/states/menu.js
@@ -18,13 +18,17 @@ Menu.prototype = {
 
     // Create game title.
     this.diseaseTxt = this.game.add.bitmapText(this.game.world.centerX,
-      (this.game.world.centerY - 200), 'new-york-escape-cond', 'Disease', 130);
+      this.game.world.centerY - 200, 'new-york-escape-cond', 'Disease', 130);
     this.diseaseTxt.anchor.set(0.5);
+    this.diseaseTxt.alpha = 0;
+
 
 
     this.defenderTxt = this.game.add.bitmapText(this.game.world.centerX,
       this.game.world.centerY -100, 'new-york-escape-cond', 'Defender', 130);
     this.defenderTxt.anchor.set(0.5);
+    this.defenderTxt.alpha = 0;
+
 
     // CLick start instruction.
     this.startTxt =  this.game.add.text(this.game.world.centerX, (this.game.world.centerY + 200),
@@ -34,10 +38,17 @@ Menu.prototype = {
       })
 
     this.startTxt.anchor.set(0.5);
+    this.startTxt.alpha = 0;
 
     // Cross
-    var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY + 50, 'bluecross-logo-large');
-    logo.anchor.set(0.5);
+    this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY + 50, 'bluecross-logo-large');
+    this.logo.anchor.set(0.5);
+    this.logo.alpha = 0;
+
+    this.game.add.tween(this.diseaseTxt).to({ alpha: 1}, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
+    this.game.add.tween(this.defenderTxt).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true,1000, 0, false);
+    this.game.add.tween(this.startTxt).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true,1500, 0, false);
+    this.game.add.tween(this.logo).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true,1500, 0, false);
   },
 
   update: function() {
