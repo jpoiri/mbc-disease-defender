e708ca0d4aaa510cd252bf928937746e5666d40e
jpoiri <justin.poirier@gmail.com>
2015-05-15 13:19:41 -0300

    commit

--- game/states/menu.js
+++ game/states/menu.js
@@ -13,41 +13,38 @@ Menu.prototype = {
   },
   create: function() {
 
-    this.diseaseTxt = "Disease";
-    this.updateIndex = 0;
-
     // Set stage color.
     this.game.stage.backgroundColor = '#FFFFFF';
 
-    for (var i = 0; i < this.diseaseTxt.length; i++) {
+    this.chars = [
+      {char: 'D', pos: [120,30]},
+      {char: 'i', pos: [214,30]},
+      {char: 's', pos: [249,30]},
+      {char: 'e', pos: [334,30]},
+      {char: 'a', pos: [422,30]},
+      {char: 's', pos: [515,30]},
+      {char: 'e', pos: [600,30]},
+      {char: 'D', pos: [0 +40,135]},
+      {char: 'e', pos: [93 +40,135]},
+      {char: 'f', pos: [181 + 40,135]},
+      {char: 'e', pos: [266 + 40,135]},
+      {char: 'n', pos: [352 + 40,135]},
+      {char: 'd', pos: [447 + 40,135]},
+      {char: 'e', pos: [540 + 40,135]},
+      {char: 'r', pos: [627 + 40,135]}
+    ]
 
-      var letter = this.game.add.bitmapText(this.game.world.centerX + (i * 100),
-        this.game.world.centerY, 'new-york-escape-cond', this.diseaseTxt.charAt(i), 130);
+    this.animatedChars = [];
 
-      letter.anchor.set(0.5);
-      letter.alpha = 0;
+    for (var i = 0; i < this.chars.length; i++) {
 
-      this.game.add.tween(letter).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true,1000, 0, false);
+      var animatedChar = this.game.add.bitmapText(this.chars[i].pos[0],
+        this.chars[i].pos[1], 'new-york-escape-cond', this.chars[i].char, 130);
+
+      animatedChar.alpha = 0;
+      this.animatedChars.push(animatedChar);
     }
 
-
-
-    // Create game title.
-    /*
-    this.diseaseTxt = this.game.add.bitmapText(this.game.world.centerX,
-      this.game.world.centerY - 200, 'new-york-escape-cond', '', 130);
-    this.diseaseTxt.anchor.set(0.5);
-    //this.diseaseTxt.alpha = 0;
-    */
-
-
-    /*
-    this.defenderTxt = this.game.add.bitmapText(this.game.world.centerX,
-      this.game.world.centerY -100, 'new-york-escape-cond', 'Defender', 130);
-    this.defenderTxt.anchor.set(0.5);
-    //this.defenderTxt.alpha = 0;
-
-*/
     // CLick start instruction.
     this.startTxt =  this.game.add.text(this.game.world.centerX, (this.game.world.centerY + 200),
       'Click to start defending', {
@@ -56,29 +53,31 @@ Menu.prototype = {
       })
 
     this.startTxt.anchor.set(0.5);
-    //this.startTxt.alpha = 0;
+    this.startTxt.alpha = 0;
 
     // Cross
     this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY + 50, 'bluecross-logo-large');
     this.logo.anchor.set(0.5);
-    //this.logo.alpha = 0;
+    this.logo.alpha = 0;
 
-    this.game.add.tween(this.diseaseTxt).to({ }, 1000, Phaser.Easing.Linear.None, true, 0, 0, false);
-    /*
-    this.game.add.tween(this.defenderTxt).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true,1000, 0, false);
-    this.game.add.tween(this.startTxt).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true,1500, 0, false);
-    this.game.add.tween(this.logo).to( { alpha: 1 }, 1000, Phaser.Easing.Linear.None, true,1500, 0, false);
-    */
+    var ellapseTime = 0;
+
+    for (var i = 0; i < this.animatedChars.length; i++) {
+      ellapseTime = 200 * i;
+      this.game.add.tween(this.animatedChars[i]).to( { alpha: 1 }, 200,
+        Phaser.Easing.Linear.None, true,ellapseTime, 0, true);
+    }
+
+    for (var i = 0; i < this.animatedChars.length; i++) {
+      this.game.add.tween(this.animatedChars[i]).to( { alpha: 1 }, 2000,
+        Phaser.Easing.Linear.None, true,ellapseTime + 1000, 0, false);
+    }
+
+    this.game.add.tween(this.startTxt).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true,ellapseTime + 1000, 0, false);
+    this.game.add.tween(this.logo).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true,ellapseTime + 1000, 0, false);
   },
 
   update: function() {
-
-    //var txt = this.diseaseTxt.text += this.testString.charAt(this.updateIndex)
-
-    ///this.diseaseTxt.setText(txt);
-
-    //this.updateIndex++;
-
     if(this.game.input.activePointer.justPressed()) {
       this.game.state.start('play');
     }
